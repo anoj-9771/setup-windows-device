50f6f69e6d043de1a0db9fecc81aab252f31e439
anoj-cha <anoj.9771@gmail.com>
2025-06-15 19:32:25 +1000

    adding uv and some other tools.

--- choco_install.bat
+++ choco_install.bat
@@ -4,6 +4,14 @@
 choco install greenshot -y
 choco install lastpass -y
 choco install powertoys -y
+choco install nerd-fonts-meslo -y
+choco install uv
+
+@REM to install ohmypash
+curl -s https://ohmyposh.dev/install.sh | bash -s
+sudo apt update
+sudo apt install unzip
+
 
 @REM to install git for windows
 winget install --id Git.Git -e --source winget
